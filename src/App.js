681be68f50c0af572858aeb2084c403ae0d83bd0
ahmedebid <ahmed-ebid@outlook.com>
2022-07-15 15:36:29 +0200

    Added the ability to save notes to localStorage

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ import Split from "react-split"
 import {nanoid} from "nanoid"
 
 export default function App() {
-    const [notes, setNotes] = React.useState([])
+
+    const [notes, setNotes] = React.useState(() => JSON.parse(localStorage.getItem("notes")) || [])
     const [currentNoteId, setCurrentNoteId] = React.useState(
         (notes[0] && notes[0].id) || ""
     )
+
+    React.useEffect(() => {
+        localStorage.setItem("notes", JSON.stringify(notes))
+    }, [notes])
     
     function createNewNote() {
         const newNote = {
