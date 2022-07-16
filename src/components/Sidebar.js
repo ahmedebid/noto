2fcc95edc31da869d8c5c663942cad62b34f3351
ahmedebid <ahmed-ebid@outlook.com>
2022-07-16 12:55:34 +0200

    Add note summary titles to the sidebar instead of just note numbers

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,21 @@
 import React from "react"
 
 export default function Sidebar(props) {
+
+    /**
+     * Challenge: Try to figure out a way to display only the 
+     * first line of note.body as the note summary in the
+     * sidebar.
+     * 
+     * Hint 1: note.body has "invisible" newline characters
+     * in the text every time there's a new line shown. E.g.
+     * the text in Note 1 is:
+     * "# Note summary\n\nBeginning of the note"
+     * 
+     * Hint 2: See if you can split the string into an array
+     * using the "\n" newline character as the divider
+     */
+
     const noteElements = props.notes.map((note, index) => (
         <div key={note.id}>
             <div
@@ -10,7 +25,7 @@ export default function Sidebar(props) {
                 }`}
                 onClick={() => props.setCurrentNoteId(note.id)}
             >
-                <h4 className="text-snippet">Note {index + 1}</h4>
+                <h4 className="text-snippet">{note.body.split("\n")[0]}</h4>
             </div>
         </div>
     ))
