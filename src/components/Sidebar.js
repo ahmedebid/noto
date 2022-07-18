fd11e989b0ee88917b2a9b1e2e6c3c95516eef26
ahmedebid <ahmed-ebid@outlook.com>
2022-07-18 14:48:40 +0200

    Add a note number next to the note summary in the Sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -11,7 +11,8 @@ export default function Sidebar(props) {
                 }`}
                 onClick={() => props.setCurrentNoteId(note.id)}
             >
-                <h4 className="text-snippet">{note.body.split("\n")[0]}</h4>
+                <h4 className="text-snippet">Note {index + 1}</h4>
+                <h4 className="note-summary">{note.body.split("\n")[0]}</h4>
                 <button 
                     className="delete-btn"
                     onClick={(event) => props.deleteNote(event, note.id)}
