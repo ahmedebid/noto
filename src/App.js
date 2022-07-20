8ff829eb4ffefcde4f07c3e2fc852a9864e2aae5
ahmedebid <ahmed-ebid@outlook.com>
2022-07-20 15:27:00 +0200

    Remove unnecessary comments

--- src/App.js
+++ src/App.js
@@ -43,18 +43,6 @@ export default function App() {
         })
     }
 
-    /**
-     * Challenge: complete and implement the deleteNote function
-     * 
-     * Hints: 
-     * 1. What array method can be used to return a new
-     *    array that has filtered out an item based 
-     *    on a condition?
-     * 2. Notice the parameters being based to the function
-     *    and think about how both of those parameters
-     *    can be passed in during the onClick event handler
-     */
-
     function deleteNote(event, noteId) {
         /* To stop propagating the click event from the trash icon to its
            parent element.
