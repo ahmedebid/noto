1622d9f6a3dec998aa93c7183b9dacdc61a674f8
ahmedebid <ahmed.ebid.dev@gmail.com>
2023-06-27 14:50:58 +0300

    Update the app to use Firebase instead of localStorage - part 2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React from "react";
 import Sidebar from "./components/Sidebar";
 import Editor from "./components/Editor";
 import Split from "react-split";
-import { onSnapshot, doc, addDoc, deleteDoc } from "firebase/firestore";
+import {
+  onSnapshot,
+  doc,
+  addDoc,
+  deleteDoc,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore";
 import { notesCollection, db } from "./firebase";
 
 export default function App() {
@@ -10,10 +17,11 @@ export default function App() {
 
   const [currentNoteId, setCurrentNoteId] = React.useState("");
 
-  console.log(currentNoteId);
-
   const currentNote =
     notes.find((note) => note.id === currentNoteId) || notes[0];
+
+  // Sort notes from most-recently-updated to least-recently-updated
+  const sortedNotes = notes.sort((a, b) => b.updatedAt - a.updatedAt);
 
   React.useEffect(() => {
     const unsubscribe = onSnapshot(notesCollection, (snapshot) => {
@@ -34,27 +42,31 @@ export default function App() {
   async function createNewNote() {
     const newNote = {
       body: "# Type your markdown note's title here",
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
     };
     const newNoteRef = await addDoc(notesCollection, newNote);
     setCurrentNoteId(newNoteRef.id);
   }
 
   // Update the current note and add it to the top of the notes list
-  function updateNote(text) {
-    setNotes((oldNotes) => {
-      const updatedNotes = [];
-      for (const oldNote of oldNotes) {
-        if (oldNote.id === currentNoteId) {
-          /* Update the current note's body text stored in the 
-                    notes state array and add the note to the top of the 
-                    notes state array */
-          updatedNotes.unshift({ ...oldNote, body: text });
-        } else {
-          updatedNotes.push(oldNote);
-        }
-      }
-      return updatedNotes;
-    });
+  async function updateNote(text) {
+    const docRef = doc(db, "notes", currentNoteId);
+    /*
+    Note:
+    =====
+      Both setDoc and updateDoc can achieve what we want here 
+      (updating the note's body) but with the following differences:
+
+      - We need to pass { merge: true } to setDoc to avoid overwriting other data
+        (in case if the note object has properties other than the "body").
+
+      - updateDoc needs to write to an existing doc (referred to by the docRef) 
+        otherwise it will fail but setDoc will create the doc if it doesn't exist.
+    */
+
+    //await setDoc(docRef, { body: text }, { merge: true });
+    await updateDoc(docRef, { body: text, updatedAt: Date.now() });
   }
 
   async function deleteNote(noteId) {
@@ -67,7 +79,7 @@ export default function App() {
       {notes.length > 0 ? (
         <Split sizes={[30, 70]} direction="horizontal" className="split">
           <Sidebar
-            notes={notes}
+            notes={sortedNotes}
             currentNote={currentNote}
             setCurrentNoteId={setCurrentNoteId}
             newNote={createNewNote}
